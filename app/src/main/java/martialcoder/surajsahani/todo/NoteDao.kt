9cfd564ffeabe928020eef7ab74c07994a6f4cdf
suraj <surajkumarsahani1997@gmail.com>
2021-08-10 09:52:18 +0530

    Room.settup..

---
+++ app/src/main/java/martialcoder/surajsahani/todo/NoteDao.kt
@@ -0,0 +1,14 @@
+package martialcoder.surajsahani.todo
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+     @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+     @Delete
+    suspend fun delete(note: Note)
+    @Query("Select * from notes_table order by id ASC")
+    fun getallnotes(): LiveData<List<Note>>
+}
---
+++ app/src/main/java/martialcoder/surajsahani/todo/NoteRepository.kt
@@ -0,0 +1,17 @@
+package martialcoder.surajsahani.todo
+
+import androidx.lifecycle.LiveData
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val  allNotes: LiveData<List<Note>> = noteDao.getallnotes()
+
+    suspend fun insert(note: Note)
+    {
+        noteDao.insert(note)
+    }
+    suspend fun delete(note: Note)
+    {
+        noteDao.delete(note)
+    }
+}
